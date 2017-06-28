e0012b19f84253551eaa9e18af56ed57e367b560
srxzr <miladnasresfahani@gmail.com>
2017-06-28 19:23:16 -0400

    npm run

--- webpack.daemon.config.js
+++ webpack.daemon.config.js
@@ -11,7 +11,7 @@ let mainConfig = {
   entry: {
     client: path.join(__dirname, 'app/src/client/main.js'),
     relay: path.join(__dirname, 'app/src/relay/main.js'),
-    control: path.join(__dirname, 'app/src/relay/control.js')
+    control: path.join(__dirname, 'app/src/client/control.js')
   },
   externals: Object.keys(pkg.dependencies || {}),
   module: {
