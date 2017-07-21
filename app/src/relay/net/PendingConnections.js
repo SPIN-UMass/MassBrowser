932d456d0b3e8839b87c8b92a5737e7602732a9f
srxzr <miladnasresfahani@gmail.com>
2017-07-21 12:28:43 -0400

    memory leak attemp 1

--- app/src/relay/net/PendingConnections.js
+++ app/src/relay/net/PendingConnections.js
@@ -11,13 +11,13 @@ class PendingConnections {
   }
 
   getPendingConnection (token) {
-    console.log(this.connections)
+    console.log('connection before:', this.connections)
     if (token in this.connections) {
       const desc = this.connections[token]
       delete (this.connections[token])
       return desc
     }
-    console.log(this.connections)
+    console.log('connection after:', this.connections)
     return false
   }
 
