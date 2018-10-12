f4f79a9720a5a3d6c6fbd0f0e3ce61f511d17082
Jerry Chai <zchai@cs.umass.edu>
2018-10-12 18:56:05 -0400

    Support appending log and also stderr log

--- deploy/one_click_deploy_relay_linux.sh
+++ deploy/one_click_deploy_relay_linux.sh
@@ -7,7 +7,7 @@ log_name="$(date +%Y-%m-%d-%H-%M-%S).log"
 touch "$log_name"
 
 if [ -f /etc/debian_version ]; then
-    echo "This is Debian based distro" | tee "$log_name"
+    echo "This is Debian based distro" | tee -a "$log_name"
     # update and install dependencies
     sudo apt-get update && sudo apt-get upgrade -y
     sudo apt-get install -y curl git build-essential
@@ -15,7 +15,7 @@ if [ -f /etc/debian_version ]; then
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
     sudo apt-get install -y nodejs
 elif [ -f /etc/redhat-release ]; then
-    echo "This is RedHat based distro" | tee "$log_name"
+    echo "This is RedHat based distro" | tee -a "$log_name"
     # update and install dependencies
     sudo yum update && sudo yum -y upgrade
     sudo yum -y install curl git gcc-c++ make
@@ -23,13 +23,13 @@ elif [ -f /etc/redhat-release ]; then
     sudo yum -y install nodejs
     curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
 else
-    echo "[ERROR] Unsupported distribution" | tee "$log_name"
+    echo "[ERROR] Unsupported distribution" | tee -a "$log_name"
     exit 1
 fi
 
 # install yarn
 
-npm install -g yarn
+npm install -g yarn 2>&1 | tee -a "$log_name"
 
 # clone MassBrowser
 if [ ! -d MassBrowser ]; then
@@ -39,10 +39,10 @@ fi
 cd MassBrowser
 
 # build MassBrowser
-yarn | tee "$log_name"
+yarn 2>&1 | tee -a "$log_name"
 
 # TODO: need to escape from the result
-yarn run watch | tee "$log_name"
+yarn run watch  2>&1 | tee -a "$log_name"
 
 # run proxy
-yarn run relay:dev | tee "$log_name" &
+yarn run relay:dev 2>&1 | tee -a "$log_name" &
