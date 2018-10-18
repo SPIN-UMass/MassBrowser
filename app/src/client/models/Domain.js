b57e52c89c3a8ce81008e71e73c1c02b2e0ccd1d
Jerry Chai <zchai@cs.umass.edu>
2018-10-17 20:12:43 -0400

    Supports recursive domain lookup in DB
    Previously, when only a single subdomain is supported, which creates a
    bug. For example, user query massbrowser.cs.umass.edu and first the
    program look for massbrowser.cs.umass.edu if it could not find it, it
    will try cs.umass.edu. Howevre, if it couldn't find cs.umass.edu, it
    assumes no rules apply. This is wrong because umass.edu may exist as a
    rule.
    
    Now it supports to query it recursively until no match can be found.
    
    Many thanks to @srxzr for spending time helping me debug and teaching
    me the best workflow for MassBrowser development.

--- app/src/client/models/Domain.js
+++ app/src/client/models/Domain.js
@@ -38,83 +38,64 @@ class DomainSchema {
    *
    */
   static findDomain (domainName) {
-    const trySubdomain = () => {
-      // Extract subdomain
-      let firstDot = domainName.indexOf('.')
-      if (firstDot == -1) {
-        return null
-      }
-
-      // i.e. split cs.umass.edu into sc. and umass.edu
-      let subdomain = domainName.substring(0, firstDot)
-      let maindomain = domainName.substring(firstDot + 1)
-
-      // Q: should we do a recursive query with subdomain extracted in
-      // each call until a matching domain is found or until the
-      // domainname is ''? Right now, if the domainname is
-      // massbrowser.cs.umass.edu, and the length of query result is
-      // 0, we then call trySubdomain which will try querying
-      // cs.umass.edu instead. But what if cs.umass.edu is not found,
-      // but we do have a rule for umass.edu availble?
+    const findDomainRec = (subdomain, maindomain) => {
       return Domain.find({name: maindomain})
         .then(domains => {
+          // if it couldn't be found in the database, try to query with
+          // its subdomain. For example, if cs.umass.edu can't be found,
+          // try umass.edu insteada.
+          if (domains.length===0) {
+          // Extract subdomain
+            var firstDot = maindomain.indexOf('.')
+            if (firstDot == -1) {
+              return null
+            }
+            // i.e. split cs.umass.edu into cs and umass.edu
+            subdomain = maindomain.substring(0, firstDot)
+            maindomain = maindomain.substring(firstDot + 1)
+
+            return findDomainRec(subdomain, maindomain)
+          }
           for (let i = 0; i < domains.length; i++) {
             // Q: Why do we need to match with subdomianRegex?
 
-            // A: the subdomainRegex is an attributes of domain
-            // object, therefore, when writing a rule in the DB, we do
-            // not have to write all the possible subdomains, rather,
-            // we write a regex instead. From the above example, we
-            // have splited cs.umass.edu into cs. and umass.edu adn
-            // query the DB with umass.edu. Assuming we have gotten a
-            // list of domain objects, we need to figure out if any of
-            // them contains a subdomainRegex that allow we have such
-            // a subdomain along withthe maindomain. That is to say,
-            // even if umass.edu object is found in database, we still
-            // need to test if cs.umass.edu is allowed in the rule.
+            // From the above example, we have splited cs.umass.edu
+            // into cs and umass.edu and query the DB with
+            // umass.edu. Assuming we have gotten a list of domain
+            // objects by querying umass.edu, we need to figure out if
+            // any of them contains a subdomain that matches cs.
+
+            // TODO: "accurate matching first" feature need to be
+            // implemented. That is to say there are two rules in the DB:
+            // {domain: umass.edu subdomain: *}
+            // {domain: umass.edu subdomain: cs}
+            // we shoud prefer matching cs rule first in this case.
             if (domains[i].subdomainRegex.test(subdomain)) {
               return domains[i]
             }
-          }
 
+            // // When a list of objects are returned, find an exact match
+            // // which has no subdomains
+            // for (let i = 0; i < domains.length; i++) {
+            //   if (!domains[i].subdomain) {
+            //     return domains[i]
+            //   }
+            // }
+
+            // // If we couldn't find an object without a subdomain field,
+            // // try finding an exact match whose subdomain matches an empty
+            // // string instead
+            // for (let i = 0; i < domains.length; i++) {
+            //   if (domains[i].subdomainRegex.test('')) {
+            //     return domains[i]
+            //   }
+            // }
+          }
           return null
-        })
-    }
-
-    // lookup the requested domainname from the database
-
-    // Domain.find will return a list of objects whose name field is
-    // domainName
-    return Domain.find({name: domainName})
-      .then(domains => {
-        // if it couldn't be found in the database, try to query with
-        // its subdomain. For example, if cs.umass.edu can't be found,
-        // try umass.edu instead.
-        if (!domains.length) {
-          return trySubdomain()
-        }
-
-        // When a list of objects are returned, find an exact match
-        // which has no subdomains
-        for (let i = 0; i < domains.length; i++) {
-          if (!domains[i].subdomain) {
-            return domains[i]
-          }
-        }
-
-        // If we couldn't find an object without a subdomain field,
-        // try finding an exact match whose subdomain matches an empty
-        // string instead
-        for (let i = 0; i < domains.length; i++) {
-          if (domains[i].subdomainRegex.test('')) {
-            return domains[i]
-          }
-        }
-
-        // No domain was found with this name that had no subdomain
-        return null
-      })
-  }
+        })                      // end .then
+    }                  // end const findDomainRec()
+    return findDomainRec('',domainName)
+  }      // end static findDomain()
 
   toString () {
     if (this.subdomain) {
