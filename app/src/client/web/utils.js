9b168945909e6668cf21c657dd72592ca1a14d79
amiiigh <ghafari.ah@gmail.com>
2018-12-06 12:59:37 -0500

    firefox for linux added

--- app/src/client/web/utils.js
+++ app/src/client/web/utils.js
@@ -1,3 +1,6 @@
+import { getDataDir } from '../../utils'
+import path from 'path'
+
 /**
  * https://stackoverflow.com/questions/2400935/browser-detection-in-javascript
  */
@@ -15,4 +18,15 @@ export function getBrowser() {
   M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
   if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
   return M[0].toLowerCase()
+}
+
+export function installCert() {
+  let certsPath = path.join(getDataDir(), '/certs/ca.pem');
+  let certName = 'MassBrowser';
+  let certDirectory = '';
+
+
+  //
+  // certdir=$(dirname ${certDB});
+  // certutil -A -n "${certificateName}" -t "TCu,Cu,Tu" -i ${certificateFile} -d sql:${certdir} --empty-password
 }
