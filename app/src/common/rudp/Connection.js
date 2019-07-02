b069e353a876207f1c68d84b5e1f7bb91904835a
amiiigh <ghafari.ah@gmail.com>
2019-07-02 11:49:19 -0400

    adding functions for the Connection to make it Duplex

--- app/src/common/rudp/Connection.js
+++ app/src/common/rudp/Connection.js
@@ -2,11 +2,13 @@ var Sender = require('./Sender');
 var Receiver = require('./Receiver');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
+var Duplex = require('stream').Duplex;
 
 // TODO: have connections refuse packets when closed.
 
 module.exports = Connection;
 function Connection(packetSender) {
+  Duplex.call(this, packetSender);
   this._sender = new Sender(packetSender);
   this._receiver = new Receiver(packetSender);
 
@@ -16,7 +18,20 @@ function Connection(packetSender) {
   });
 };
 
-util.inherits(Connection, EventEmitter);
+// util.inherits(Connection, EventEmitter);
+util.inherits(Connection, Duplex);
+
+Connection.prototype._write = function (chunk, encoding, callback) {
+
+}
+
+Connection.prototype._read = function () {
+
+}
+
+Connection.prototype._final = function () {
+  this.push(null);
+}
 
 Connection.prototype.send = function (data) {
   this._sender.send(data);
