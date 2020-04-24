9406061a8d0f4cbc7aab32daf2a6dfec8e19508b
amiiigh <ghafari.ah@gmail.com>
2020-04-24 14:19:03 -0400

    rupd encry

--- app/src/common/rudp/Connection.js
+++ app/src/common/rudp/Connection.js
@@ -121,7 +121,7 @@ Connection.prototype.incrementNextExpectedSequenceNumber = async function () {
 Connection.prototype._decrypt = function(encryptedPacketWithIV) {
   let iv = encryptedPacketWithIV.slice(0,16);
   let key = this._packetSender._sessionKey.slice(0,32);
-  console.log('key', key)
+  console.log(this._packetSender._sessionKey.slice(0,32).toString('hex'))
   console.log('iv:', iv);
   let encryptedPacket = encryptedPacketWithIV.slice(16);
   let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
