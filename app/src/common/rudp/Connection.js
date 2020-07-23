fc2644a210f09cdd6f3b37f0160c81d82244d0fc
amiiigh <ghafari.ah@gmail.com>
2020-07-23 12:14:49 -0400

    fix cls cls

--- app/src/common/rudp/Connection.js
+++ app/src/common/rudp/Connection.js
@@ -99,6 +99,7 @@ Connection.prototype._stopTimeoutTimer = function () {
 
 Connection.prototype._startTimeoutTimer = function () {
   this._connectionTimeoutTimer = setTimeout(() => {
+    debug('TIMEOUT',  this._packetSender.getAddressKey())
     this._changeCurrentTCPState(constants.TCPStates.CLOSED);
     this._sender._stopTimeoutTimer();
     this._sender.clear();
@@ -111,6 +112,7 @@ Connection.prototype._startTimeoutTimer = function () {
 }
 
 Connection.prototype._restartTimeoutTimer = function () {
+  debug('RESET TIMEOUT',  this._packetSender.getAddressKey())
   this._stopTimeoutTimer();
   this._startTimeoutTimer();
 }
@@ -191,14 +193,15 @@ Connection.prototype.send = async function (data) {
 };
 
 Connection.prototype.receive = async function (buffer) {
-    if (this.currentTCPState === constants.TCPStates.CLOSED) {
-      debug('CONNECTION IS CLOSED RECEIVE IGNORED')
-      return
-    }
     let release = await this._receiverLock.acquire()
     let packet = new Packet(buffer)
     if (this._packetSender._sessionKey) {
       packet = new Packet(this._decrypt(buffer))
+    }
+    if (this.currentTCPState === constants.TCPStates.CLOSED) {
+      debug('CONNECTION IS CLOSED RECEIVE IGNORED')
+      release();
+      return
     }
     this._restartTimeoutTimer();
     switch(this.currentTCPState) {
